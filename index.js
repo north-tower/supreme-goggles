a63ef6f50ad0e0786cf562a5f6480196c421b020
Miki <muneshmyke@gmail.com>
2024-05-18 14:40:53 +0300

    010

--- index.js
+++ index.js
@@ -66,12 +66,12 @@ app.get('/api/v1/getTrips', async (req, res) => {
 // Endpoint to write data (add a new driver) to Firestore
 app.post('/api/v1/addDriver', async (req, res) => {
   try {
-    const { description, amount, category } = req.body;
+    const { id ,description, amount, category } = req.body;
   app.post('/api/v1/addDriver', async (req, res) => {
   try {
     const { description, amount, category } = req.body;
     // Assuming the data to be written is sent in the request body
-    await admin.firestore().collection('expense').add({ description, amount, category  });
+    await admin.firestore().collection('expense').add({id, description, amount, category  });
     res.status(201).send('Expense added successfully');
   } catch (error) {
     console.error('Error adding new driver to Firestore:', error);

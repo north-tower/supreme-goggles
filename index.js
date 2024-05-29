1fb3ff69054dcbdad455b18b600ef51f07d484ca
Miki <muneshmyke@gmail.com>
2024-05-29 16:31:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -112,13 +112,13 @@ app.get('/api/v1/getIncome/:start/:end', async (req, res) => {
     const { start, end } = req.params;
 
     // Parse the times to Firestore Timestamp objects
-   // const startTimestamp = admin.firestore.Timestamp.fromDate(new Date(start));
-    //const endTimestamp = admin.firestore.Timestamp.fromDate(new Date(end));
+    const startTimestamp = admin.firestore.Timestamp.fromDate(new Date(start));
+    const endTimestamp = admin.firestore.Timestamp.fromDate(new Date(end));
 
     // Create a query with the time range
     const snapshot = await admin.firestore().collection('expense')
-      .where('createdAt', '>=', '2024-01-01')
-      .where('createdAt', '<=', '2024-06-01')
+      .where('createdAt', '>=', startTimestamp)
+      .where('createdAt', '<=', endTimestamp)
       .get();
     const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     res.json(data);

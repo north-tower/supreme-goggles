2adf331282002103d5c339924773cbe4b3d198a3
Miki <muneshmyke@gmail.com>
2024-05-29 17:55:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -127,6 +127,26 @@ app.get('/api/v1/getIncome/:start/:end', async (req, res) => {
     res.status(500).send('Error reading data from Firestore');
   }
 });
+app.get('/api/v1/getExpense/:start/:end', async (req, res) => {
+  try {
+    const { start, end } = req.params;
+
+    // Parse the times to Firestore Timestamp objects
+    const startTimestamp = admin.firestore.Timestamp.fromDate(new Date(start));
+    const endTimestamp = admin.firestore.Timestamp.fromDate(new Date(end));
+
+    // Create a query with the time range
+    const snapshot = await admin.firestore().collection('expense')
+      .where('createdAt', '>=', startTimestamp)
+      .where('createdAt', '<=', endTimestamp)
+      .get();
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.json(data);
+  } catch (error) {
+    console.error('Error reading data from Firestore:', error);
+    res.status(500).send('Error reading data from Firestore');
+  }
+});
 
 app.get('/api/v1/getCategories', async (req, res) => {
   try {
